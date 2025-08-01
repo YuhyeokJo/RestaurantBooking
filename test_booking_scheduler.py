aef44ba01475cf1f62de93f62766b2bfa180c060
yuhyeok <joyu0320@naver.com>
2025-08-01 13:12:01 +0900

    [refactor] step12

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -8,26 +8,21 @@ from test_communication import TestableSmsSender, TestableMailSender
 CUSTOMER = Customer("Fake name", "010-1234-5678")
 NOT_ON_THE_HOUR = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
 ON_THE_HOUR = datetime.strptime("2021/03/26 09:00", "%Y/%m/%d %H:%M")
+SUNDAY = "2021/03/28 17:00"
+MONDAY = "2024/06/03 17:00"
 CUSTOMER_WITH_MAIL = Customer("Fake name", "010-1234-5678", "test@test.com")
 
 UNDER_CAPACITY = 1
 CAPACITY_PER_HOUR = 3
 
 
-class SundayBookingScheduler(BookingScheduler):
-    def __init__(self, capacity_per_hour):
+class TestableBookingScheduler(BookingScheduler):
+    def __init__(self, capacity_per_hour, date_time: str):
         super().__init__(capacity_per_hour)
+        self._date_time = date_time
 
     def get_now(self):
-        return datetime.strptime("2021/03/28 17:00", "%Y/%m/%d %H:%M")
-
-
-class MondayBookingScheduler(BookingScheduler):
-    def __init__(self, capacity_per_hour):
-        super().__init__(capacity_per_hour)
-
-    def get_now(self):
-        return datetime.strptime("2024/06/03 17:00", "%Y/%m/%d %H:%M")
+        return datetime.strptime(self._date_time, "%Y/%m/%d %H:%M")
 
 
 @pytest.fixture
@@ -123,7 +118,7 @@ def test_이메일이_있는_경우에는_이메일_발송(booking_scheduler_wit
 
 def test_현재날짜가_일요일인_경우_예약불가_예외처리():
     # arrange
-    booking_scheduler = SundayBookingScheduler(CAPACITY_PER_HOUR)
+    booking_scheduler = TestableBookingScheduler(CAPACITY_PER_HOUR, SUNDAY)
     schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
     # act and assert
     with pytest.raises(ValueError):
@@ -132,7 +127,7 @@ def test_현재날짜가_일요일인_경우_예약불가_예외처리():
 
 def test_현재날짜가_일요일이_아닌경우_예약가능():
     # arrange
-    booking_scheduler = MondayBookingScheduler(CAPACITY_PER_HOUR)
+    booking_scheduler = TestableBookingScheduler(CAPACITY_PER_HOUR, MONDAY)
     schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
     # act
     booking_scheduler.add_schedule(schedule)
